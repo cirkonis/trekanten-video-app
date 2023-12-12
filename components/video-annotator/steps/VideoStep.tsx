c92ba68629a6efcb068c7a02e85384ea50b372cf
Michael Marigliano <102356266+mimar126@users.noreply.github.com>
2023-12-12 15:11:40 +0100

    changed a bit

--- components/video-annotator/steps/VideoStep.tsx
+++ components/video-annotator/steps/VideoStep.tsx
@@ -2,10 +2,10 @@ import {useVideoStore} from "@/state/videoState";
 import React from "react";
 import Player from "react-player";
 import {Simulate} from "react-dom/test-utils";
-import playing = Simulate.playing;
 import {useStepStore} from "@/state/annotationStepsState";
+import playing = Simulate.playing;
 
-export function VideoStep(){
+export function VideoStep() {
     const uploadedVideo = useVideoStore((state) => state.url);
     const setUploadedVideo = useVideoStore((state) => state.setUploadedVideo);
     const playerRef = useVideoStore((state) => state.playerRef);
@@ -36,34 +36,41 @@ export function VideoStep(){
     return (
         <div>
             <div className="flex items-center w-full justify-start">
-            <h2 className="px-8 text-xl">Set video title:</h2>
-            <input type="text" placeholder="Fencer's Fencing"
-                   id="video-title"
-                   className="input input-bordered w-full max-w-xs"
-                   value={videoTitle}
-                   onChange={(e) => setVideoTitle(e.target.value)}
-                   required
-            />
+                <h2 className="px-8 text-xl">Set video title:</h2>
+                <input type="text" placeholder="Fencer's Fencing"
+                       id="video-form"
+                       className="input input-bordered w-full max-w-xs"
+                       value={videoTitle}
+                       onChange={(e) => setVideoTitle(e.target.value)}
+                       required
+                />
             </div>
-            <div className="border border-pink-500 border-2 w-full flex flex-col flex-grow justify-end items-center my-8 p-16 min-h-[500px]">
-            {uploadedVideo && (
-                <div>
-                    <Player
-                        ref={playerRef}
-                        url={uploadedVideo}
-                        onPlaying={playing}
-                        controls={true}
-                    />
-                </div>
-            )}
-            <input
-                type="file"
-                accept="video/*"
-                className="file-input file-input-bordered w-full max-w-xs"
-                onChange={handleVideoUpload}
-            />
+            <div className="w-full flex flex-col justify-evenly items-center my-8 p-16 min-h-[500px]">
+                {!uploadedVideo && (
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-32 h-32">
+                            <path strokeLinecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"/>
+                        </svg>
+                    </div>
+                )}
+                {uploadedVideo && (
+                    <div>
+                        <Player
+                            ref={playerRef}
+                            url={uploadedVideo}
+                            onPlaying={playing}
+                            controls={true}
+                        />
+                    </div>
+                )}
+                <input
+                    type="file"
+                    accept="video/*"
+                    className="mt-6 file-input file-input-bordered w-full max-w-xs"
+                    onChange={handleVideoUpload}
+                />
             </div>
-                {/* Conditionally set the disabled attribute based on videoTitle and uploadedVideo */}
+            {/* Conditionally set the disabled attribute based on videoTitle and uploadedVideo */}
             <div className="px-8 flex w-full justify-end">
                 <button
                     className="btn btn-primary"
