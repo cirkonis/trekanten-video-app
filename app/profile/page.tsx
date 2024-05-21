2b5c93ef770ebcd5ccd5b2b82304dedfeff4f89d
Michael Marigliano <43167596+cirkonis@users.noreply.github.com>
2024-05-21 14:44:19 +0200

    set the app to deploy on app engine instead (#15)
    Co-authored-by: Michael Marigliano <102356266+mimar126@users.noreply.github.com>

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,27 +1,27 @@
-import { getServerSession } from "next-auth";
-import Link from "next/link";
+// import { getServerSession } from "next-auth";
+// import Link from "next/link";
 
 export default async function Profile() {
-    const user = await getServerSession();
+    // const user = await getServerSession();
 
     return (
         <>
             <section className="bg-ct-blue-600  min-h-screen pt-20">
                 <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
-                    <div>
-                        <Link href="/video">
-                        <p className="mb-3 text-5xl text-center font-semibold">
-                            Let's Go 🤺 🎥
-                        </p>
-                            </Link>
-                        {!user ? (
-                            <p>Loading...</p>
-                        ) : (
-                            <div className="flex justify-evenly items-center gap-8">
-                                    ☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿
-                            </div>
-                        )}
-                    </div>
+                    {/*<div>*/}
+                    {/*    <Link href="/video">*/}
+                    {/*    <p className="mb-3 text-5xl text-center font-semibold">*/}
+                    {/*        Let's Go 🤺 🎥*/}
+                    {/*    </p>*/}
+                    {/*        </Link>*/}
+                    {/*    {!user ? (*/}
+                    {/*        <p>Loading...</p>*/}
+                    {/*    ) : (*/}
+                    {/*        <div className="flex justify-evenly items-center gap-8">*/}
+                    {/*                ☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿☝🏿*/}
+                    {/*        </div>*/}
+                    {/*    )}*/}
+                    {/*</div>*/}
                 </div>
             </section>
         </>
